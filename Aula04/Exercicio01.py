1dd81fc32b96941248a983484622e2d3fa854614
isabelmaito <maitoisabel@gmail.com>
2024-03-31 03:29:35 -0300

    Exercicio 01

---
+++ Aula04/Exercicio01.py
@@ -0,0 +1,16 @@
+print("Olá mundo!")
+
+print(6+5)
+print("6"+"5")
+x = 5
+print(x)
+y = 6
+print("Resultado da soma:", x+y)
+
+print(x, y)
+x, y = y, x
+print(x, y)
+
+nome = "Maria"
+idade = 25
+print(nome, "tem", idade, "anos!")
