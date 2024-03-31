0eb4fa4db177351b4d0afb01056fcd4b41fcf0ab
isabelmaito <maitoisabel@gmail.com>
2024-03-31 03:52:32 -0300

    Exercicio 03

--- Aula04/Exericio03.py
+++ Aula04/Exericio03.py
@@ -1,3 +1,3 @@
-x = int(input("Digite o valor de x: "))
-y = int(input("Digite o valor de y: "))
-print("Resultado da soma: ", x+y)
+x = int(input("Digite o valor de x:"))
+y = int(input("Digite o valor de y:"))
+print("Resultado da soma:", x+y)
