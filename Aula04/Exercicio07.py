3d6cdeaef2b4aaaba5e971f6ac488dbd0f36973a
isabelmaito <maitoisabel@gmail.com>
2024-03-31 22:34:12 -0300

    Exercicio 07 - Calculadora de salário com entrada de porcentagem.

---
+++ Aula04/Exercicio07.py
@@ -0,0 +1,4 @@
+salario = int(input("Digite seu salário:"))
+aumento = int(input("Digite quantos % de aumento:"))
+sal_aumento = salario * (1+(aumento/100))
+print("Salário com aumento de ",aumento,"%:", sal_aumento)
