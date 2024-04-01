6966fb4c1624d2afad47bd01349a580d27e16575
isabelmaito <maitoisabel@gmail.com>
2024-04-01 01:02:03 -0300

    Exercicio 11 - Conversão de medidas

---
+++ Aula04/Exercicio11.py
@@ -0,0 +1,8 @@
+pes = int(input("Digite o valor em pés:"))
+pol = pes * 12
+jar = pes / 3
+milha = jar / 1760
+
+print("Valor em polegadas:", pol)
+print("Valor em jardas:", jar)
+print("Valor em milhas:", milha)
