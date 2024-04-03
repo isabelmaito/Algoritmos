e79741509d83f942c02e6462aea6db13da6ba7b2
isabelmaito <maitoisabel@gmail.com>
2024-04-03 03:04:27 -0300

    Exercicio 01 - Divisor comum entre 5 e 3.

---
+++ Aula06/Exercicio01.py
@@ -0,0 +1,6 @@
+numero = int(input("Digite algum valor:"))
+
+if (numero % 5 == 0) and (numero % 3 == 0):
+    print("Esse número é divisível por 5 e 3")
+else:
+    print("Esse número NÃO é divisível por 5 e 3")
